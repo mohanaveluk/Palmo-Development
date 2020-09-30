c995a253cde3ceeff6d45b68f0dc7249f1911f25
Mohanavelu <mohanaveluk@gmail.com>
2020-09-30 14:12:18 -0500

    updated about page and SCO content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Palmo Home', sitelogo:'Palmo' });
+  res.render('index', { title: '100+ plano milling job work in Ambattur Industrial Estate, machiniing & Slotting process', sitelogo:'Palmo' });
+});
+
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: '100+ plano milling job work in Ambattur Industrial Estate, machiniing & Slotting process', sitelogo:'Palmo' });
 });
 
 /* GET home page. */
